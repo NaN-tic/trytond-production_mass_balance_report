0e2d7cacc9c52902ddcf109ac9cccf4eab33e1b9
Raimon Esteve <raimonesteve@gmail.com>
2020-05-13 10:17:15 +0200

    Generate setup.py from generate-setup nankit

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
-# This file is part production_mass_balance_report module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# encoding: utf-8
 
 from setuptools import setup
 import re
 import os
 import io
-try:
-    from configparser import ConfigParser
-except ImportError:
-    from ConfigParser import ConfigParser
+from configparser import ConfigParser
 
-MODULE2PREFIX = {}
+MODULE = 'production_mass_balance_report'
+PREFIX = 'nantic'
+MODULE2PREFIX = {'html_report': 'trytonspain'}
+OWNER = {
+    'nantic':'NaN-tic',
+    'trytonzz':'nanticzz',
+}
 
 
 def read(fname):
@@ -30,51 +31,82 @@ def get_require_version(name):
         major_version, minor_version + 1)
     return require
 
+def get_requires(depends='depends'):
+  requires = []
+  for dep in info.get(depends, []):
+      if not re.match(r'(ir|res)(\W|$)', dep):
+          prefix = MODULE2PREFIX.get(dep, 'trytond')
+          owner = OWNER.get(prefix, prefix)
+          if prefix == 'trytond':
+              requires.append(get_require_version('%s_%s' % (prefix, dep)))
+          else:
+              requires.append(
+                  '%(prefix)s-%(dep)s@git+https://github.com/%(owner)s/'
+                  'trytond-%(dep)s.git@%(branch)s'
+                  '#egg=%(prefix)s-%(dep)s-%(series)s'%{
+                          'prefix': prefix,
+                          'owner': owner,
+                          'dep':dep,
+                          'branch': branch,
+                          'series': series,})
+
+  return requires
+
 config = ConfigParser()
 config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
+
 version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'nantic_production_mass_balance_report'
-download_url = "https://github.com/NaN-tic/trytond-production_mass_balance_report"
 
 requires = []
-for dep in info.get('depends', []):
-    if not re.match(r'(ir|res)(\W|$)', dep):
-        prefix = MODULE2PREFIX.get(dep, 'trytond')
-        requires.append(get_require_version('%s_%s' % (prefix, dep)))
-requires.append(get_require_version('trytond'))
 
-tests_require = []
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'master'
+else:
+    branch = series
+
+requires += get_requires('depends')
+
+tests_require = [
+    get_require_version('proteus'),
+    
+    ]
+tests_require += get_requires('extras_depend')
+requires += [get_require_version('trytond_production')]
+
 dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='Tryton Production Mass Balance Report Module',
+    description='',
     long_description=read('README'),
     author='NaN-tic',
-    author_email='info@nan-tic.com',
-    url='https://github.com/NaN-tic/',
-    download_url=download_url,
-    keywords='',
-    package_dir={'trytond.modules.production_mass_balance_report': '.'},
+    url='http://www.nan-tic.com/',
+    download_url='https://github.com:NaN-tic/trytond-production_mass_balance_report',
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.production_mass_balance_report',
-        'trytond.modules.production_mass_balance_report.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.production_mass_balance_report': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml',
+            'icons/*.svg']),
         },
+    project_urls = {
+       "Source Code": 'https://github.com:NaN-tic/trytond-production_mass_balance_report'
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -87,12 +119,8 @@ setup(name=name,
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
@@ -101,10 +129,10 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    production_mass_balance_report = trytond.modules.production_mass_balance_report
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
+
     )
